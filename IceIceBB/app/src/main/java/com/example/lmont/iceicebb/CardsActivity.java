2e814fa04940b1884552c9e4b97b498b8034d064
pauldebellis <pvdebellis@gmail.com>
2016-09-23 16:07:44 -0700

    Adds empty activities

---
+++ IceIceBB/app/src/main/java/com/example/lmont/iceicebb/CardsActivity.java
@@ -0,0 +1,13 @@
+package com.example.lmont.iceicebb;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CardsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cards);
+    }
+}
