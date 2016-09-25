ac3dd6beb47f750c45c572aeb5006d5bce9e267c
Gabe Keyner <holyground32@gmail.com>
2016-09-24 22:12:09 -0700

    applied splash screen

--- IceIceBB/app/src/main/java/com/example/lmont/iceicebb/FeelingLuckyActivity.java
+++ IceIceBB/app/src/main/java/com/example/lmont/iceicebb/FeelingLuckyActivity.java
@@ -1,13 +1,17 @@
 package com.example.lmont.iceicebb;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class FeelingLuckyActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_feeling_lucky);
+
+
+
     }
 }
---
+++ IceIceBB/app/src/main/java/com/example/lmont/iceicebb/splashscreen.java
@@ -0,0 +1,62 @@
+package com.example.lmont.iceicebb;
+
+/**
+ * Created by GabeKeyner on 9/24/2016.
+ */
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Window;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+
+public class splashscreen extends AppCompatActivity {
+
+
+    //Set waktu lama splashscreen
+    private static int splashInterval = 3000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        setContentView(R.layout.splashscreen);
+        ImageView splashView = (ImageView)findViewById(R.id.splashView);
+
+        final Animation anim = AnimationUtils.loadAnimation(splashscreen.this,R.anim.fade);
+        splashView.startAnimation(anim);
+
+
+        new Handler().postDelayed(new Runnable() {
+
+
+            @Override
+            public void run() {
+                // TODO Auto-generated method stub
+
+
+                Intent i = new Intent(splashscreen.this, GameActivity.class);
+                startActivity(i);
+
+
+
+                this.finish();
+            }
+
+            private void finish() {
+                // TODO Auto-generated method stub
+
+            }
+        }, splashInterval);
+
+    };
+
+}
