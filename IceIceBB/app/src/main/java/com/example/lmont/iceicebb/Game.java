c512b3d0a02e43fb028f3b66f32c0eddd7f6c007
Leo Montegrande <lmontegrande91@aol.com>
2016-09-25 12:58:39 -0700

    - Adds classes to set up SyncAdapter

---
+++ IceIceBB/app/src/main/java/com/example/lmont/iceicebb/Game.java
@@ -0,0 +1,21 @@
+package com.example.lmont.iceicebb;
+
+/**
+ * Created by lmont on 9/25/2016.
+ */
+public class Game {
+    // To create a new game, for now, just use postman to do a post to
+    // https://floating-island-55807.herokuapp.com/games
+
+    public String name;
+    public String comment;
+    public String rules;
+    public boolean isClean;
+    public boolean hasCards;
+    public boolean hasDice;
+    public String tags;
+    public int minPlayers;
+    public int maxPlayers;
+    public String materials;
+    public int rating;
+}
