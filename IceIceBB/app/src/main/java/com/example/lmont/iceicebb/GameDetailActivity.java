e756bcdcfec9ee27f418e7f5b6595b45943d89f5
pauldebellis <pvdebellis@gmail.com>
2016-09-27 16:19:56 -0700

    adds descriptive toasts for each tag in game detail

--- IceIceBB/app/src/main/java/com/example/lmont/iceicebb/GameDetailActivity.java
+++ IceIceBB/app/src/main/java/com/example/lmont/iceicebb/GameDetailActivity.java
@@ -9,6 +9,7 @@ import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.RatingBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class GameDetailActivity extends AppCompatActivity {
 
@@ -24,17 +25,20 @@ public class GameDetailActivity extends AppCompatActivity {
         game = dbHelper.getGameWithName(name);
 
         TextView gameName = (TextView)findViewById(R.id.gameNameDetail);
-        ImageView playersIcon = (ImageView)findViewById(R.id.playersIcon);
         TextView playerCount = (TextView)findViewById(R.id.playerCount);
         ImageView sfwIcon = (ImageView)findViewById(R.id.sfwIcon);
         RatingBar ratingBar = (RatingBar)findViewById(R.id.ratingBarDetail);
-        LinearLayout tagFrameDetail = (LinearLayout)findViewById(R.id.tagFrameDetail);
         TextView gameMaterials = (TextView)findViewById(R.id.gameMaterials);
         TextView gameRules = (TextView)findViewById(R.id.gameRules);
         TextView gameComments = (TextView)findViewById(R.id.gameComments);
         Button diceRollerButton = (Button)findViewById(R.id.diceRollerButton);
-        Button cardDeckButton = (Button)findViewById(R.id.cardFlipperButton);
+        final Button cardDeckButton = (Button)findViewById(R.id.cardFlipperButton);
 
+        final LinearLayout tagHolder = (LinearLayout) findViewById(R.id.tagFrameDetail);
+        ImageView drinkIcon = (ImageView) findViewById(R.id.drinkIcon);
+        ImageView movingIcon = (ImageView) findViewById(R.id.movingIcon);
+        ImageView carIcon = (ImageView) findViewById(R.id.carIcon);
+        final ImageView paperIcon = (ImageView) findViewById(R.id.paperIcon);
 
 
 //        Float ratingFloat = Float.valueOf(getIntent().getStringExtra("rating"));
@@ -53,7 +57,7 @@ public class GameDetailActivity extends AppCompatActivity {
         if (!game.hasCards){
             cardDeckButton.setVisibility(View.GONE);
         }
-
+//ONCLICKS to send user to appropriate tool activity
         diceRollerButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -69,15 +73,50 @@ public class GameDetailActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-
+//IF elses (there's probably a DRYer way) to set SFW icon + visibility of tags
         if (game.isClean){
             sfwIcon.setImageDrawable(getResources().getDrawable(R.drawable.angel));
         }
         else {
             sfwIcon.setImageDrawable(getResources().getDrawable(R.drawable.devil));
         }
-//TODO: check what tags are present and toggle visibility of appropriate tag icons
-//        if (game.tags)
+//TODO: check what tags are present in GAME object and toggle visibility of appropriate tag icons
+        if (game.tags.contains("drinking")){
+            drinkIcon.setVisibility(View.VISIBLE);
+        }
+        if (game.tags.contains("movement")){
+            movingIcon.setVisibility(View.VISIBLE);
+        }
+        if (game.tags.contains("car")){
+            carIcon.setVisibility(View.VISIBLE);
+        }
+        if (game.tags.contains("writing")){
+            paperIcon.setVisibility(View.VISIBLE);
+        }
+
+        tagHolder.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                switch (view.getId()){
+                    case R.id.drinkIcon:
+                        Toast drinkToast = Toast.makeText(getApplicationContext(), "This game works well with booze", Toast.LENGTH_SHORT);
+                        drinkToast.show();
+                        break;
+                    case R.id.movingIcon:
+                        Toast movingToast = Toast.makeText(getApplicationContext(), "This game requires a bit of physical activity", Toast.LENGTH_SHORT);
+                        movingToast.show();
+                        break;
+                    case R.id.carIcon:
+                        Toast carToast = Toast.makeText(getApplicationContext(), "This is an ideal game to play in a car", Toast.LENGTH_SHORT);
+                        carToast.show();
+                        break;
+                    case R.id.paperIcon:
+                        Toast paperToast = Toast.makeText(getApplicationContext(), "This game requires a pen and paper", Toast.LENGTH_SHORT);
+                        paperToast.show();
+                        break;
+                }
+            }
+        });
 
     }
 }
