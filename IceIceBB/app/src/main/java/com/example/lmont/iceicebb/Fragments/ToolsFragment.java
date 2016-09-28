e30768fdf976814f3dc93542483a149df6eda9fc
Spencer Richmond <spencerrichmondschulman@gmail.com>
2016-09-28 09:32:37 -0700

    Making card and dice buttons to go to activity. Commented out because they work differently in fragments.

--- IceIceBB/app/src/main/java/com/example/lmont/iceicebb/Fragments/ToolsFragment.java
+++ IceIceBB/app/src/main/java/com/example/lmont/iceicebb/Fragments/ToolsFragment.java
@@ -6,6 +6,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.example.lmont.iceicebb.R;
 
@@ -14,8 +15,8 @@ import com.example.lmont.iceicebb.R;
  */
 public class ToolsFragment extends Fragment {
 
-
-
+    Button cardToolButton;
+    Button diceToolButton;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -23,6 +24,25 @@ public class ToolsFragment extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_tools, container, false);
 
+        //TODO: Find out if this needs to be different inside a fragment...
+//        //Set button to direct to Card tool activity
+//        cardToolButton = (Button) view.findViewById(cardToolButton);
+//        cardToolButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Intent intent = new Intent(ToolsFragment.this, CardsActivity.class);
+//                startActivity(intent);
+//            }
+//        });
+//        //Set button to direct to Dice tool activity
+//        diceToolButton = (Button) view.findViewById(R.id.diceToolButton);
+//        diceToolButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Intent intent = new Intent(ToolsFragment.this, DiceActivity.class);
+//                startActivity(intent);
+//            }
+//        });
 
         return view;
     }
