e18d7acc14e00e6640f1d731d882216945e08996
Spencer Richmond <spencerrichmondschulman@gmail.com>
2016-09-28 09:51:43 -0700

    Finishes making the card and dice buttons direct from fragment to the respective activities.

--- IceIceBB/app/src/main/java/com/example/lmont/iceicebb/Fragments/ToolsFragment.java
+++ IceIceBB/app/src/main/java/com/example/lmont/iceicebb/Fragments/ToolsFragment.java
@@ -1,6 +1,7 @@
 package com.example.lmont.iceicebb.Fragments;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -8,6 +9,8 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 
+import com.example.lmont.iceicebb.CardsActivity;
+import com.example.lmont.iceicebb.DiceActivity;
 import com.example.lmont.iceicebb.R;
 
 /**
@@ -25,24 +28,24 @@ public class ToolsFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_tools, container, false);
 
         //TODO: Find out if this needs to be different inside a fragment...
-//        //Set button to direct to Card tool activity
-//        cardToolButton = (Button) view.findViewById(cardToolButton);
-//        cardToolButton.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Intent intent = new Intent(ToolsFragment.this, CardsActivity.class);
-//                startActivity(intent);
-//            }
-//        });
-//        //Set button to direct to Dice tool activity
-//        diceToolButton = (Button) view.findViewById(R.id.diceToolButton);
-//        diceToolButton.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Intent intent = new Intent(ToolsFragment.this, DiceActivity.class);
-//                startActivity(intent);
-//            }
-//        });
+        //Set button to direct to Card tool activity
+        cardToolButton = (Button) view.findViewById(R.id.cardToolButton);
+        cardToolButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getContext(), CardsActivity.class);
+                startActivity(intent);
+            }
+        });
+        //Set button to direct to Dice tool activity
+        diceToolButton = (Button) view.findViewById(R.id.diceToolButton);
+        diceToolButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getContext(), DiceActivity.class);
+                startActivity(intent);
+            }
+        });
 
         return view;
     }
